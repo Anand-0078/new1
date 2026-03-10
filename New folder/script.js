866811c581cff02e09f991b4231cdd1f01a0328c
anand0078_Design <116070487+Anand-0078@users.noreply.github.com>
2026-03-10 19:03:15 +0530

    Refactor navigation menu toggle functionality
    Updated event listener to change navigation link colors and fixed variable name.

--- New folder/script.js
+++ New folder/script.js
@@ -1,23 +1,27 @@
 const menuIcon = document.querySelector(".close-icon");
 const navMenu = document.querySelector(".navigation");
-const navMenuu = document.querySelector(".navigation a");
-const social= document.querySelector(".social-interaction");
+const navLinks = document.querySelectorAll(".navigation a");
+const social = document.querySelector(".social-interaction");
 
 menuIcon.addEventListener("click", function () {
 
     if (navMenu.style.display === "flex") {
         navMenu.style.display = "none";
-        navMenu.style.backgroundColor = "#008DD5"; // sky blue
+        navMenu.style.backgroundColor = "#008DD5";
     } else {
         navMenu.style.display = "flex";
         navMenu.style.flexDirection = "column";
         navMenu.style.backgroundColor = "yellow";
         navMenu.style.padding = "70px";
-        naviMenuu.style.color = "black";
-        social.style.display ="flex";
+
+        // Change navigation link color
+        navLinks.forEach(link => {
+            link.style.color = "black";
+        });
+
+        social.style.display = "flex";
         social.style.flexDirection = "row";
         social.style.paddingTop = "405px";
     }
-
 
 });
